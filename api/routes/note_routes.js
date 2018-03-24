cf15b2825840217d277b1efdfab5ae9003f84194
Thomasguillaume <thomaseq.guillaume@gmail.com>
2018-03-24 10:12:41 +0100

    feat(es): index in bulk all car records

--- api/routes/note_routes.js
+++ api/routes/note_routes.js
@@ -1,20 +1,53 @@
+const elasticsearch = require('elasticsearch');
 const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
 
+
+var elasticClient = new elasticsearch.Client({
+    host: '192.168.99.100:9200',
+    log: 'trace'
+});
+
+
 async function populate() {
   const brands = await getBrands();
-  console.log(brands);
+  var body = [];
+  var compteur = 1;
   for(var i=0; i<brands.length; i++)
   {
     const models = await getModels(brands[i]);
-    console.log(models);
+    for(var j=0; j<models.length; j++)
+    {
+      body.push({
+        index:
+        {
+          _index: 'caradisiac',
+          _type: 'car',
+          _id: compteur
+        }
+      });
+      body.push(models[j]);
+      compteur++;
+    }
   }
+  elasticClient.bulk({
+      body: body
+  }, function (error, response) {
+      if (error) {
+          console.error(error);
+          return;
+      }
+      else {
+          console.log(response);
+      }
+  });
 }
+
 
 module.exports = function(app, db) {
   app.post('/populate', (req, res) => {
     populate()
-    res.send('Index Records to ElasticSearch')
+    res.send('Indexing Records to ElasticSearch')
   });
   app.post('/suv', (req, res) => {
     // You'll create your note here.
---
+++ server.js
@@ -0,0 +1,12 @@
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const app           = express();
+const port          = 9292;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./api/routes')(app, {});
+
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
